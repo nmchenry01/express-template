e32c0aadc21aa69aace91d9c3dce9762e47ffd9e
nmchenry01 <nicholas.mchenry@levvel.io>
2019-08-27 18:19:01 -0700

    feat(middleware): add first iteration of middleware

--- app.js
+++ app.js
@@ -4,3 +4,23 @@
     - Should be relatively minimal
     - Instantiates express API, adds middleware, uses routes, connects to database, starts up server, etc
 */
+
+const { json, urlencoded } = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Disable x-powered-by header for security
+app.disable('x-powered-by');
+
+// Instantiate middleware
+app.use(helmet());
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('combined'));
+
+app.listen(8080, () => console.log('Hello World!'));
